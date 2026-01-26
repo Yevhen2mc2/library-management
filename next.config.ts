b061319cbf2de7863c3005755ac71e5a372f9b6c
Yevhen <yevhen.o@2muchcoffee.com>
2026-01-26 18:32:08 +0200

    setup react compiler, update zod, update eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   images: {
     remotePatterns: [new URL("https://covers.openlibrary.org/**")],
   },
